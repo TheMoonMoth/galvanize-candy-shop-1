0b81092d7e0dd317605bf4b1ff0a542f921ab387
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-11-10 13:20:24 -0700

    Refactored code to be more compact and readable

--- drills/version-1/exercise.js
+++ drills/version-1/exercise.js
@@ -18,45 +18,21 @@ function candySalesToObject(candy){
 {});
 }
 
-
-function salesDayToObject(day, saleObj){
-  var dayObj = {date: day, sales: []};
-  var saleValues = Object.values(saleObj);
-  for (var i = 0; i < saleValues[0].length; i++) {
-    dayObj.sales.push(saleValues[0][i].reduce(function(accumulator, item, index){
-      switch(index){
-        case 0:
-          accumulator.item = item;
-          break;
-        case 1:
-          accumulator.price = item;
-          break;
-        case 2:
-          accumulator.quantity = item;
-          break;
-        default:
-          break;
-      }
-    return accumulator;
-    },{}));
-  }
-  return dayObj;
+function salesDayToObject(date, saleObj){
+  var dateObj = {
+    date: date,
+    sales: saleObj[date].map(candySalesToObject)
+  };
+  return dateObj;
 }
 
 
-function allSalesToArray(completeObj){
-  var finalSalesArray = [];
-  var dateKeys = Object.keys(completeObj);
-  dateKeys.map(function(unit){
-    var dayObj = {date: unit, sales: []};
-    completeObj[unit].map(function(item){
-      var oneItem = {item: item[0], price: item[1], quantity: item[2]};
-      dayObj.sales.push(oneItem);
-    });
-    finalSalesArray.push(dayObj);
+function allSalesToArray(completeObj) {
+  return Object.keys(completeObj).map(function(oneDate){
+    return salesDayToObject(oneDate, completeObj);
   });
-return finalSalesArray;
 }
+
 
 module.exports = {
     // Uncomment these functions as you write them
