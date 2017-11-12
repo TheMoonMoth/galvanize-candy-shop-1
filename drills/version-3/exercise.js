5f4fe9a328cc56b72ea28d31d605b59f5cd97d39
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-11-12 10:14:07 -0700

    Adding refactored function code | Passing

--- drills/version-3/exercise.js
+++ drills/version-3/exercise.js
@@ -1,6 +1,32 @@
+function getTotalItemSales(saleList, itemId){
+  return saleList.reduce(function(totaler, each){
+    if (each.itemId === itemId) {
+      return totaler + each.quantity;
+    } else {
+      return totaler;
+    }
+  }, 0);
+}
+
+function addTotalSalesToItems(saleList, itemList){
+  return itemList.map(function(each){
+    each.quantity = getTotalItemSales(saleList, each.id);
+    return each;
+  });
+}
+
+function addTotalValueToItems(saleList, itemList){
+  return addTotalSalesToItems(saleList, itemList).map(function(each){
+    each.totalValue = (Number(each.quantity) * Number(each.price)).toFixed(2);
+    return each;
+  });
+}
+
+
+
 module.exports = {
     // Uncomment these functions as you write them
-    // getTotalItemSales,
-    // addTotalSalesToItems,
-    // addTotalValueToItems
+    getTotalItemSales,
+    addTotalSalesToItems,
+    addTotalValueToItems
 }
